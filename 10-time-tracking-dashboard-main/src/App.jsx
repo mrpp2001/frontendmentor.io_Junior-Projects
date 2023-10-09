a316492cd1f65fc82a8cd0d787a1030452299e83
Pratik Patil <pratiknpatil2001@gmail.com>
2023-10-09 15:49:54 +0530

    Add files via upload

---
+++ 10-time-tracking-dashboard-main/src/App.jsx
@@ -0,0 +1,13 @@
+import Profile from "./components/Profile";
+
+function App() {
+  return (
+    <>
+      <section className="sm:flex justify-center sm:gap-7 mt-20 ">
+        <Profile />
+      </section>
+    </>
+  );
+}
+
+export default App;
